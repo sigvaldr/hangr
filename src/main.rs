b0e9ecc4760600a875c8241eaff7d2d829717796
Sigvaldr <sigvaldrnotthrafn@proton.me>
2024-11-13 17:49:59 -0500

    implemented WORD_LIST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// Wordlist
+const WORD_LIST: [&str; 100] = [
+    "adventure", "backyard", "campfire", "diplomat", "euphoria", "freckles", "geometry", "handmade",
+    "isolated", "jubilant", "keyboard", "lifetime", "magician", "necklace", "overcome", "paradise",
+    "question", "recharge", "sandwich", "together", "umbrella", "vacation", "warhorse", "xenolith",
+    "yardstick", "zealotry", "balconies", "cartoons", "dialogue", "elephant", "florists", "grapevine",
+    "harmless", "islander", "jumpstart", "knowledge", "landmine", "magazine", "nightmare", "overtake",
+    "postcard", "quilters", "radiator", "sapphire", "teaspoon", "underdog", "villager", "waterloo",
+    "xylophone", "yearbook", "zippered", "abstract", "bachelor", "calendar", "dinosaur", "eleventh",
+    "feedback", "giraffes", "hospital", "intranet", "junction", "keyboard", "laughter", "medicine",
+    "narrator", "octangle", "portrait", "question", "rainfall", "skeleton", "triangle", "universe",
+    "volcanoe", "whiplash", "xenonate", "yielding", "zoologic", "activity", "backpack", "compound",
+    "deserted", "engraver", "fishbowl", "grandson", "hairline", "icebound", "jumpable", "kneecaps",
+    "livening", "monogram", "nickname", "obdurate", "panicked", "quadrant", "reformer", "sunshine",
+    "tethered", "unworthy", "vintages", "webisode"
+];
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", WORD_LIST[99]);
 }
